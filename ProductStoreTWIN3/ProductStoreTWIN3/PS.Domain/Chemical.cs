feeb360c988940a9cb81f6c64da3134254b01201
Elwafi Hiba <73645403+HibaEF@users.noreply.github.com>
2022-03-02 12:02:09 +0100

    first commmit

---
+++ ProductStoreTWIN3/ProductStoreTWIN3/PS.Domain/Chemical.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PS.Domain
+{
+   public class Chemical : Product
+    {
+        public String City { get; set; }
+        public String LabName { get; set; }
+        public String StreetAddress { get; set; }
+        public override void GetMyType()
+        {
+            //Console.WriteLine("Je suis un produit");
+            base.GetMyType();
+            Console.WriteLine(" Chemical");
+        }
+        public override string ToString()
+        {
+            return base.ToString()+"City :"+City;
+        }
+    }
+    
+   
+
+}
---
+++ ProductStoreTWIN3/ProductStoreTWIN3/PS.Domain/Product.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PS.Domain
+{
+
+    //sealed pour bloquer l heritage
+    public class Product
+    {
+        public int ProductId { get; set; }
+        public String Name { get; set; }
+        public DateTime DateProd { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public string Description  { get; set; }
+        public Category Category { get; set; }
+        public int ImageName { get; set; }
+        public List<Provider> Providers { get; set; }
+
+        public override string ToString()
+        {
+            return "Name: "+Name+" DateProd: "+DateProd+" Quantity: "+Quantity+" Price: "+Price+" Category :"+Category+" Providers :"+Providers;
+        }
+
+        public virtual void GetMyType()
+        {
+            Console.Write("Je suis un produit");
+        }
+
+
+
+    }
+}
